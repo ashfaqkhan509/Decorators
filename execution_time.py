d9949cbf8991c22a7ddb27b86423ef71b06a7b55
ashi <ashfaq319535@gmail.com>
2025-07-03 21:16:24 +0500

    Python Foramt cheker

--- execution_time.py
+++ execution_time.py
@@ -4,8 +4,6 @@ Apply this decorator to various functions and compare their execution times."""
 
 
 import time
-
-
 def timing(func):
     def wrapper(*args, **kwargs):
         start = time.time()
@@ -16,16 +14,11 @@ def timing(func):
 
     return wrapper
 
-
 @timing
 def loop():
     for _ in range(500000):
         pass
-
-
 loop()
-
-
 @timing
 def const_fun(name):
     return f"My name is {name}"
